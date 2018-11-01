76c6ed6eb622d991c488dfc084946f28792dc974
Benas <b@Benass-MacBook-Pro.local>
2018-11-01 15:24:40 +0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -23,9 +23,10 @@ app.post('/todos', (req,res)=>{
   }, (e)=>{
       res.status(400).send(e);
   });
-
 });
 
 app.listen(3000, ()=>{
   console.log("Started on port 3000");
 })
+
+module.exports ={app};
