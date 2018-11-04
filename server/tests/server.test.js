b1ce768547172ddeb29283bff6e7a57f77aafff0
Benas <b@Benass-MacBook-Pro.local>
2018-11-04 23:11:52 +0200

    Added Delete/ Test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -113,3 +113,46 @@ describe('GET /todos/:id', ()=>{
   })
 
 });
+
+
+describe('DELETE /todos/:id', ()=>{
+
+  it('should remove a todo', (done) =>{
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+
+      Todo.findById(hexId).then((res)=>{
+        expect(res).toBeNull();
+        done();
+      }).catch((e)=>{
+        done(e);
+      });
+    });
+  });
+
+  it('should return 404 if todo not found', (done)=>{
+    request(app)
+    .delete('/todos/5bdb1c165570351f206a33f9')
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done)=>{
+    request(app)
+    .delete('/todos/c70351f206a33f9')
+    .expect(404)
+    .end(done);
+  });
+
+
+});
