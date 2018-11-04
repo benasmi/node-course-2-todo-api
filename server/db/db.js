764b6a48f5927365d4c23edd43d1609f75926b05
Benas <b@Benass-MacBook-Pro.local>
2018-11-04 14:45:10 +0200

    Heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://node:BenasMiliunas123@ds249873.mlab.com:49873/node_api');
 
 module.exports = {
   mongoose: mongoose
