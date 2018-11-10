90b09aca6f7ac6e1b9000f56ffceca0280cbd2ac
Benas <b@Benass-MacBook-Pro.local>
2018-11-10 10:37:37 +0200

    PATCH /todos/ route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 //Library imports
 var {ObjectID} = require('mongodb')
+var _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 //Local imports
@@ -54,6 +55,7 @@ app.get('/todos/:id', (req,res)=>{
   });
 });
 
+//Delete todos
 app.delete('/todos/:id', (req,res) =>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
@@ -67,6 +69,35 @@ app.delete('/todos/:id', (req,res) =>{
   }).catch((e)=>{
     res.status(400).send();
   });
+});
+
+//Update todos
+app.patch('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
+
+
 });
 
 app.listen(port, ()=>{
