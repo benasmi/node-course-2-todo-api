166aa03e622581c37d4c2bc78fdbb9b947d15021
Benas <b@Benass-MacBook-Pro.local>
2018-11-10 15:25:59 +0200

    s

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://node:BenasMiliunas123@ds249873.mlab.com:49873/node_api');
 
 module.exports = {
   mongoose: mongoose
